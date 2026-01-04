0d6c3b3db3d854f9565a9b0c0921826145441129
Raman Choudhary <raman.choudhary65@gmail.com>
2026-01-04 12:58:58 +0530

    feat: update ecosystem configuration and add PM2 commands for process management

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -9,10 +9,7 @@ module.exports = {
       autorestart: true,
       watch: false,
       max_memory_restart: "1G",
-      env_production: {
-        NODE_ENV: "production",
-        PORT: 3000,
-      },
+      env_file: "./.env",
       error_file: "./logs/pm2-error.log",
       out_file: "./logs/pm2-out.log",
       log_date_format: "YYYY-MM-DD HH:mm:ss Z",
